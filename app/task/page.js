db7653fc51ba043828cc6074081d471a0b69d6d5
ankitsejwal <ankit@sejw.al>
2023-11-04 00:41:40 +0530

    added states to task page

--- app/task/page.js
+++ app/task/page.js
@@ -1,41 +1,66 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 export default function Page() {
+  const [taskName, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
+  const [project, setProject] = useState(null);
+  const [priority, setPriority] = useState(null);
+  const [dueDate, setDueDate] = useState(null);
+
   return (
-    <div className="flex h-screen justify-center ">
-      <form className="flex flex-col mt-20 w-80 space-y-4">
+    <div className="flex h-screen justify-center">
+      <form className="flex flex-col mt-20 w-80 space-y-4" onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
-          placeholder="Task Title"
+          placeholder="Enter task"
           className="p-3 rounded-lg bg-[#3B3B3B]"
+          onChange={(e) => setTaskName(e.target.value)}
         />
         <textarea
           className="p-3 h-20 rounded-lg bg-[#3B3B3B]"
           placeholder="Description"
-        ></textarea>
-
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <select
           name="Project"
           className="p-3 rounded-lg bg-[#3B3B3B] text-[#999]"
+          onChange={(e) => setProject(e.target.value)}
         >
           <option value="" selected disabled>
             Select project
           </option>
-          <option>A</option>
-          <option>B</option>
-          <option>C</option>
+          <option>solsense</option>
+          <option>to-do</option>
+          <option>Haayi</option>
         </select>
         <div className="space-x-3 flex">
           <input
             placeholder="Priority"
-            className="p-3 w-28 rounded-lg bg-[#3B3B3B]"
+            className="p-3 w-28 rounded-lg bg-[#3B3B3B] text-center"
+            onChange={(e) => setPriority(e.target.value)}
+            value={priority}
           />
-
-          <button className="p-3 bg-pink-900 w-20 rounded-lg">1</button>
-          <button className="p-3 bg-pink-900 w-20 rounded-lg">2</button>
-          <button className="p-3 bg-pink-900 w-20 rounded-lg">3</button>
+          <button className="p-3 bg-pink-900 w-20 rounded-lg" onClick={() => setPriority(1)}>
+            1
+          </button>
+          <button className="p-3 bg-pink-900 w-20 rounded-lg" onClick={() => setPriority(2)}>
+            2
+          </button>
+          <button className="p-3 bg-pink-900 w-20 rounded-lg" onClick={() => setPriority(3)}>
+            3
+          </button>
         </div>
-        <input type="date" className="p-3 rounded-lg bg-[#3B3B3B]" />
+        <input
+          type="date"
+          className="p-3 rounded-lg bg-[#3B3B3B]"
+          onChange={(e) => setDueDate(e.target.value)}
+        />
+        {taskName}
+        {description}
+        {project}
+        {priority}
+        {dueDate}
         <button className="p-3 bg-pink-900 rounded-lg">+ add task</button>
       </form>
     </div>
